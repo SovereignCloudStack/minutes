666309220a4f7fcb3ee30026f02bedb09dca34ce
Kurt Garloff <kurt@garloff.de>
2024-12-08 16:50:44 +0100

    Use download API /download to grab .md files.
    Name them .md then also for better rendering.
    Replace links to hedgedoc by link to the local .md file.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- tools/check_links.sh
+++ tools/check_links.sh
@@ -44,12 +44,15 @@ if test ! -r "$1"; then echo "ERROR: File \"$1\" not readable" 1>&2; exit 2; fi
 
 ADDS=""
 errs=0
+
+SEDCHANGES=""
 while test -n "$1"; do
 INPATH=${1%/*}
 INFILE=${1##*/}
-pushd .
+pushd . >/dev/null 2>&1
 if test "$INPATH" != "$1"; then cd $INPATH; INPATH="${INPATH}/"; else INPATH=""; fi
 
+CHANGES=""
 while read line; do
 	# FIXME: Handle multiple links in one line
 	LINK="$(echo $line | sed 's@^.*\(https://input.scs.community/[^) #?"]*\).*$@\1@')"
@@ -60,8 +63,21 @@ while read line; do
 		else
 			echo "$LINK missing"
 			if test "$DOWNLOAD" = "1"; then
-				curl -LO "$LINK"
-				if test $? = "0"; then echo "Downloaded $LINK successfully."; ADDS="$ADDS ${INPATH}${LINK##*/}"
+				if ispic $LINK; then
+					curl -LO "$LINK"
+					ERR=$?
+					TGTFILE=${LINK##*/}
+				else
+					curl -LO "$LINK"/download
+					ERR=$?
+					TGTFILE=${LINK##*/}.md
+					mv download $TGTFILE
+				fi
+				if test $ERR = "0"; then
+					echo "Downloaded $LINK successfully."
+					ADDS="$ADDS ${INPATH}${LINK##*/}"
+					LINK="$(echo $line | sed 's@^.*\(https://input.scs.community/[^) #"]*\).*$@\1@')"
+					CHANGES="$CHANGES -e 's~$LINK~./$TGTFILE~g'"
 				else echo "ERROR downloading $LINK" 1>&2; let errs+=1; fi
 			else
 				let errs+=1
@@ -69,12 +85,14 @@ while read line; do
 		fi
 	fi
 done < <(tr ' ' '\n' < "$INFILE"|grep 'https://input.scs.community'|sed 's/!\[..*\]//g')
+if test -n "$CHANGES"; then SEDCHANGES="$SEDCHANGES sed -i $CHANGES $1;"; fi
 shift
-popd
+popd >/dev/null 2>&1
 done
 
 if test "$DOWNLOAD" -a -n "$ADDS"; then
 	echo -e "Consider\ngit add $ADDS"
+	echo "$SEDCHANGES"
 fi
 if test $errs -gt 0; then
 	echo "$errs missing files" 1>&2
